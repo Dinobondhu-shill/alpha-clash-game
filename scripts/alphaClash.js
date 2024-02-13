55a0c638c089433ac228113ab8862b8023933bb1
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-02-14 02:43:22 +0600

    get random number and display any random word and set bg

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -1,4 +1,18 @@
+
+
+function startGame (){
+ const alphabets = getAlphabet();
+// display the randomly generated alphabates to the screen
+const screenWord = document.getElementById('display-word');
+screenWord.innerText = alphabets;
+
+// set bgcolor to the screen word
+setBgColorById(alphabets);
+
+}
+
 function play (){
   hideElement('home_screen');
-  showElement('playground')
+  showElement('playground');
+  startGame();
 }
--- scripts/utilities.js
+++ scripts/utilities.js
@@ -6,3 +6,20 @@ function showElement (elementById){
   const element = document.getElementById(elementById);
   element.classList.remove('hidden');
 }
+// getting random alphabets 
+function getAlphabet(){
+  const alphabetsString = 'abcdefghijklmnopqrstuvwxyz'
+  const alphabets = alphabetsString.split('');
+
+  
+  const randomIndex = Math.random() *25;
+  const index = Math.round(randomIndex);
+  
+  const word = alphabets[index];
+  return word;
+}
+// set background color to the highlighted alphabate
+function setBgColorById (element){
+  const elementId = document.getElementById(element);
+  elementId.classList.add('bg-orange-400');
+}
