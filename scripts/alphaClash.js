e7e621b20187049a91c55964057fb02e9ae3eb1f
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-02-14 23:51:33 +0600

    simple matching function added

--- scripts/alphaClash.js
+++ scripts/alphaClash.js
@@ -1,4 +1,22 @@
 
+function handlePressedKeyEvent(event){
+  // get the key which is pressed by the player
+  const playerPressedKey = event.key;
+  console.log(playerPressedKey);
+  // get the key which need to press the player
+  const expectedKeyId = document.getElementById('display-word');
+  const expectedKey = expectedKeyId.innerText;
+  const exactKey = expectedKey.toLowerCase();
+// match the key between player pressed and expected key
+
+if(playerPressedKey === exactKey){
+ startGame();
+}
+else{
+  console.log('you lost a lifeline');
+}
+}
+document.addEventListener('keyup', handlePressedKeyEvent);
 
 function startGame (){
  const alphabets = getAlphabet();
@@ -8,7 +26,6 @@ screenWord.innerText = alphabets;
 
 // set bgcolor to the screen word
 setBgColorById(alphabets);
-
 }
 
 function play (){
